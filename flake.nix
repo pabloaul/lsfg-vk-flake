382a1614c007506ee36cb02abe05b6631575b4aa
Nojus <git@nojus.org>
2025-07-21 23:52:21 +0200

    flake.nix: add arm64 as system
    Closes: #5

--- flake.nix
+++ flake.nix
@@ -1,13 +1,15 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      packages.${system}.default = pkgs.callPackage ./default.nix { };
-      nixosModules.default = import ./module.nix;
-    };
+  outputs = { nixpkgs, ... }: 
+  let
+    forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ];
+  in 
+  {
+    packages = forAllSystems (system:
+      { default = nixpkgs.legacyPackages.${system}.callPackage ./default.nix { }; }
+    );
+
+    nixosModules.default = import ./module.nix;
+  };
 }
